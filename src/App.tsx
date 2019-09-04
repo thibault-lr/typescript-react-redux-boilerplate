73f3d11109cfad920c51bf1163e242515609362f
thibault.leroy <thibault.le-roy@saegus.com>
2019-09-04 17:45:49 +0200

    change some component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 
-export const Sample = () => <h1>Test</h1>
+export const Sample = () => <h1>Test 2</h1>
 
 ReactDOM.render(React.createElement(Sample), document.getElementById("root"))
