36f9bb9ee0fcd6c431ae27262219f35c0f8887a0
thibault.leroy <thibault.le-roy@saegus.com>
2019-09-25 10:19:22 +0200

    improve unit tests on components

---
+++ src/pages/Blog/Blog.test.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import { mount } from 'enzyme';
+import Blog from 'pages/Blog/Blog';
+import { Header } from 'components/molecules/Header';
+
+import { MemoryRouter } from 'react-router';
+
+
+describe.only('Blog', () => {
+  it('Should render the blog page correctly', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <Blog />
+      </MemoryRouter>,
+    );
+
+    expect(wrapper.find(Header).length).toBe(1);
+  });
+});
